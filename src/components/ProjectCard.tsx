9135b385d171b0941de7a4699fb329c95a9ab72b
foxerinc <dediyanto.ti@gmail.com>
2025-05-26 15:30:41 +0700

    Update ProjectCard.tsx
    add transisition

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Project } from '../types';
+import { FaArrowRight } from 'react-icons/fa';
 
 interface ProjectCardProps {
   project: Project;
@@ -8,28 +9,27 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   return (
-    <article className="bg-[#1a1a1a] rounded-xl border border-border overflow-hidden transition-transform hover:-translate-y-2 hover:shadow-[0_8px_20px_rgba(0,255,255,0.25)]">
+      <div className="bg-[#1a1a1a] border border-secondary rounded-xl overflow-hidden flex flex-col h-full min-h-[350px]">
       <Image src={project.image} alt={`${project.title} Image`} width={320} height={250} className="w-full h-[250px] object-cover rounded-t-xl" />
       <div className="p-6">
-        <h3 className="text-2xl xs:text-xl font-heading mb-2 font-bold">{project.title}</h3>
-        <p className="text-secondaryText text-lg xs:text-base mb-3">{project.description}</p>
-        <ul className="flex flex-wrap gap-2 mb-3">
-          {project.tech.map((tech, index) => (
-            <li key={index} className="bg-border px-3 py-1 rounded-md text-base xs:text-sm">{tech}</li>
+        <h3 className="text-2xl xs:text-xl font-heading mb-2 font-bold line-clamp-2">{project.title}</h3>
+
+        <ul className="flex flex-wrap gap-1.5 mb-3">
+          {project.tech.slice(0,3).map((tech, index) => (
+            <li key={index} className="bg-border border px-3 py-1 rounded-md text-base xs:text-sm">{tech}</li>
           ))}
         </ul>
-            <div className="flex justify-start">
-              <Link
-                href={project.codeUrl}
-                className="inline-block px-6 py-3 border-2 border-tertiary text-secondary rounded-md text-lg xs:text-base hover:bg-tertiary hover:text-secondary hover:shadow-[0_0_10px_rgba(0,255,255,0.5)] hover:scale-105 transition-all duration-300"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Code
-              </Link>
+            <p className="text-secondaryText text-lg xs:text-base mb-3 line-clamp-3">{project.description}</p>
+            <div className="flex justify-end">
+                <Link
+                  href={`/detail/${project.id}`}
+                  className="inline-flex items-center gap-5 p-2 font-semibold text-primary text-lg xs:text-base hover:text-tertiary transition-all duration-300"
+                >
+                  See Detail <FaArrowRight />
+                </Link>
             </div>
       </div>
-    </article>
+    </div>
   );
 };
 
